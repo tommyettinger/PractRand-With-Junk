9ef2537ed27013ffe1eac3b5fb4eaed32df94abc
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-08-14 13:45:02 -0700

    Figured out a better way to set aesrand's stream

--- src/RNGs/other/special.cpp
+++ src/RNGs/other/special.cpp
@@ -15,12 +15,19 @@ namespace PractRand {
 		namespace Polymorphic {
 			namespace NotRecommended {
                 /*
-                Passes 64TB with one unusual anomaly at 256GB, when tested without the change to increment (which is also the stream).
+                Passes 64TB with no anomalies with stream constantly 0, PractRand seed 1.
                 With the increment constantly changing, this has one anomaly at 512GB instead, and still passes 64TB.
                 The increment shouldn't change by adding an odd number; that would result in an even increment and that halves the period, or worse.
-                I use 0x9E3779B97F4A7C16 here instead of the pi-based number; its last bits are 0b10, which allows 2 to the 63 streams.
-                More streams are possible by adding a different number to the otherwise-unchanged first 64 bits of increment.
-                The other number should probably also end in 0b10, but shouldn't be added every time the later 64 bits are changed.
+                The failures that aesrand normally gets at 8TB when incrementing only the low 64-bits of the state are quick to remedy.
+                If you have a 64-bit stream value, you can split it up into two 32-bit sections, then set only  the upper halves of each 64-bit section.
+                This can be done with:
+
+                increment = _mm_set_epi32(streamA, 0xF9F87D4D, streamB, 0x9E3779B9);
+
+                Where streamA and streamB are each 32-bit halves of the 64-bit stream. This has been tested with streamA=0 and streamB=0, which had
+                no anomalies through 64TB of testing. Because the increment must have each 64-bit half be an odd number to ensure the generator is
+                full-period (it seems to not pass testing if one of the increment halves is even, as well), this only allows changing the upper bits,
+                which certainly seems to work well.
                 */
                 Uint64 aesdragontamer::raw64() {
                     if((idx++) == 0){
@@ -31,7 +38,7 @@ namespace PractRand {
                         __m256i full = _mm256_insertf128_si256(_mm256_castsi128_si256(penultimate2), penultimate1, 1);
                         _mm256_storeu_si256((__m256i *) buf, full);
                         ////Tested this too, it works to at least 64TB, constantly changing seed.
-                        increment = _mm_add_epi64(increment, _mm_set_epi64x(0u, 0x9E3779B97F4A7C16u));
+                        //increment = _mm_add_epi64(increment, _mm_set_epi64x(0u, 0x9E3779B97F4A7C16u));
                     }
                     return buf[idx &= 3];
                 }
@@ -40,7 +47,8 @@ namespace PractRand {
                 }
                 void aesdragontamer::walk_state(StateWalkingObject *walker) {
                     idx = 0;
-                    increment = _mm_set_epi64x(0xCB9C59B3F9F87D4Du, 0x3463A64C060782B1u);
+                    increment = _mm_set_epi32(0, 0xF9F87D4D, 0, 0x9E3779B9);
+                    //increment = _mm_set_epi64x(0xCB9C59B3F9F87D4Du, 0x3463A64C060782B1u);
 //                  increment = _mm_set_epi8(0x2f, 0x2b, 0x29, 0x25, 0x1f, 0x1d, 0x17, 0x13, 
 //                        0x11, 0x0D, 0x0B, 0x07, 0x05, 0x03, 0x02, 0x01);
                     Uint64 seed1, seed2;
