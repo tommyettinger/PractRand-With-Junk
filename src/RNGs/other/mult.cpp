412f9e899d22b4c612d8e0e17a3b4dd198e7f9c7
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-08 21:43:09 -0800

    Another one bites the dust.

--- src/RNGs/other/mult.cpp
+++ src/RNGs/other/mult.cpp
@@ -3831,9 +3831,10 @@ namespace PractRand {
 	const uint64_t fb = stateB;
 	const uint64_t fc = stateC;
 	const uint64_t fd = stateD;
-    //// Good rotations: 11-13, 19, 22-23, 26, 37-38, 41-42, 45-46, 50, 53-55
+    //// Good rotations: 11-13, 19, 22, 26, 37-38, 41-42, 45-46, 50, 53-55
+	//// 23 gets two mildly suspicious at 4TB
 	//// 39 gets a mildly suspicious at 8TB
-	stateA = rotate64(fc + fb, 23);
+	stateA = rotate64(fc + fb, 45);
 	stateB = fc ^ fd;
 	stateC = fa + fb;
 	stateD = fd + 0x9E3779B97F4A7C15UL;
